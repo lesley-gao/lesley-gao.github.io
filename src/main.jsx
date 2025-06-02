224b89ac2d12e7c12a1207bb840ac35ac1da9c15
Lesley <glei164@aucklanduni.ac.nz>
2025-06-03 00:11:51 +1200

    add hotjar tag

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import Hotjar from '@hotjar/browser';
+
+const siteId = 6422647;
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
